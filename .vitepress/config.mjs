1cefa01e96d185d7b5ea73b497668f9c82f1eb36
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2024-04-06 21:36:24 +0200

    Create config.mjs

---
+++ .vitepress/config.mjs
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Cosmos-dao",
+  description: "Cosmos-dao",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Documentation', link: '/documentation/' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Start',
+        items: [
+          { text: 'Group module concepts', link: '/documentation/' },
+          { text: 'Group Metadata', link: '/documentation/start/metadata' },
+          { text: 'Create group', link: '/documentation/start/create-group' },
+          { text: 'Manage group', link: '/documentation/start/manage-group' },
+          { text: 'Create proposal', link: '/documentation/start/create-proposal' }
+        ]
+      },
+      {
+        text: 'Our network',
+        items: [
+          { text: 'Chain config', link: '/documentation/chain-config' },
+          { text: 'LCD endpoint', link: 'https://lcd-devnet.cosmos-dao.com/' },
+          { text: 'RPC endpoint', link: 'https://rpc-devnet.cosmos-dao.com/' } 
+        ]        
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer: {
+      message: 'Dev by atmon3r, Bitcanna-Lab, Cosmos-dao team',
+      //copyright: 'Dev by <a href="https://github.com/yyx990803">Bitcanna-Lab</a>'
+    },
+    /* search: {
+      provider: 'local'
+    } */
+  }
+})
